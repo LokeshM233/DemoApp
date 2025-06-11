4f1d230d5bf6e04f1e5d6a92266b050019a286a8
LokeshM233 <machanilokesh.233@gmail.com>
2025-06-11 12:41:00 +0530

    First Commit

---
+++ DemoApp/src/in/lokesh/main/DemoApp.java
@@ -0,0 +1,19 @@
+package in.lokesh.main;
+
+public class DemoApp {
+
+	public static void main(String[] args) {
+		int[] arr = { 1, 2, 3, 4, 5 };
+		for (int i = 0; i < arr.length; i++) {
+			System.out.println("Element at index " + i + ": " + arr[i]);
+		}
+		//for each loop
+//		for (int element : arr) {
+//			System.out.println("Element: " + element);
+//		}
+		String name = "LokeshM!";
+		int couunt = name.toUpperCase().length();
+		System.out.println("Length of the string in uppercase: " + couunt);
+	}
+
+}
